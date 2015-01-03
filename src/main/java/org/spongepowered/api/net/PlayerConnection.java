d97d33c787a2d5a6c736567cb5f374c4be7ccd1a
Gabriel Harris-Rouquette <gabizou@me.com>
2015-01-03 12:52:40 -0800

    Clarify javadocs on PlayerConnection#getHostName.

--- src/main/java/org/spongepowered/api/net/PlayerConnection.java
+++ src/main/java/org/spongepowered/api/net/PlayerConnection.java
@@ -49,19 +49,22 @@ public interface PlayerConnection {
     InetAddress getAddress();
 
     /**
-     * Gets the host name of the connection.
+     * Gets the host name the connection is connecting to the server with.
+     *
+     * <p>Examples include: If a player is connecting to `mc.example.com`,
+     * the hostname will show `mc.example.com`. This is NOT the originating
+     * hostname of the client.</p>
      *
      * @return The host name
      */
     String getHostname();
 
     /**
-     * Pings the connection and returns the duration in milliseconds for the
-     * round trip from server to client and back to server.
+     * Gets the connection ping. This is constantly calculated by the server.
      *
      * @return The ping
      */
-    int ping();
+    int getPing();
 
     /**
      * Sends a custom payload to the player connection.
